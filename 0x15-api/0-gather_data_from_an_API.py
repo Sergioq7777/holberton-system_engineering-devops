4cbb0c2c5a7550acf798602b935dd16e3188a3a3
sergioq7777 <sergioq.7777@gmail..com>
2020-05-04 22:56:13 -0500

    gather data

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" REST API = gather info """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    parameters_t = (('userId', sys.argv[1]),)
+    to_dos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos',
+        params=parameters_t)
+    to_dos_j = to_dos.json()
+
+    user = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1]))
+    user_j = user.json()
+    user_name = user_j['name']
+
+    completed_list = []
+    for maybe in to_dos_j:
+        if maybe['completed'] is True:
+            completed_list.append(maybe)
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        user_name,
+        len(completed_list),
+        len(to_dos_j)))
+    for title in completed_list:
+        print("\t {}".format(title['title']))
